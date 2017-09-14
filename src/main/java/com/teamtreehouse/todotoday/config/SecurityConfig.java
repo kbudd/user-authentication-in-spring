7dea62b986083fdf2babdfa9e0dc882b7d258b83
kylebudd <thekbudd@gmail.com>
2017-09-14 11:32:41 -0400

    Updates made through final video -- should now match master

--- src/main/java/com/teamtreehouse/todotoday/config/SecurityConfig.java
+++ src/main/java/com/teamtreehouse/todotoday/config/SecurityConfig.java
@@ -2,13 +2,22 @@ package com.teamtreehouse.todotoday.config;
 
 import com.teamtreehouse.todotoday.service.UserService;
 import com.teamtreehouse.todotoday.web.FlashMessage;
+import org.omg.CORBA.Object;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.repository.query.spi.EvaluationContextExtension;
+import org.springframework.data.repository.query.spi.EvaluationContextExtensionSupport;
+import org.springframework.security.access.expression.SecurityExpressionRoot;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -23,7 +32,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-        auth.userDetailsService(userService);
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
     }
 
     @Override
@@ -45,7 +59,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .logout()
                 .permitAll()
-                .logoutSuccessUrl("/login");
+                .logoutSuccessUrl("/login")
+                .and()
+            .csrf();
     }
 
     public AuthenticationSuccessHandler loginSuccessHandler() {
@@ -56,6 +72,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return (request, response, exception) -> {
             request.getSession().setAttribute("flash", new FlashMessage("Incorrect username/password. Please try again.", FlashMessage.Status.FAILURE));
             response.sendRedirect("/login");
+        };
+    }
+
+    @Bean
+    public EvaluationContextExtension securityExtension() {
+        return new EvaluationContextExtensionSupport() {
+            @Override
+            public String getExtensionId() {
+                return "security";
+            }
+
+            @Override
+            public java.lang.Object getRootObject() {
+                Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                return new SecurityExpressionRoot(authentication) {};
+            }
         };
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,12 +1,12 @@
 -- Insert role
 insert into role (name) values ('ROLE_USER')
 
--- Insert two users
-insert into user (username,enabled,password,role_id) values ('user',true,'password',1);
-insert into user (username,enabled,password,role_id) values ('user2',true,'password',1);
+-- Insert two users (passwords are both 'password')
+insert into user (username,enabled,password,role_id) values ('user',true,'$2a$06$g9TnpfgGkiGm0BiC/PnRTOQES8iPSYQ1NF6XlKyKwlTL1RaO1fJka',1);
+insert into user (username,enabled,password,role_id) values ('user2',true,'$2a$06$g9TnpfgGkiGm0BiC/PnRTOQES8iPSYQ1NF6XlKyKwlTL1RaO1fJka',1);
 
 -- Insert tasks
-insert into task (complete,description) values (true,'Code Task entity');
-insert into task (complete,description) values (false,'Discuss users and roles');
-insert into task (complete,description) values (false,'Enable Spring Security');
-insert into task (complete,description) values (false,'Test application');
+insert into task (complete,description,user_id) values (true,'Code Task entity',1);
+insert into task (complete,description,user_id) values (false,'Discuss users and roles',2);
+insert into task (complete,description,user_id) values (false,'Enable Spring Security',1);
+insert into task (complete,description,user_id) values (false,'Test application',2);
